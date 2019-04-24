67ee42ded1a9ba21834fb5adf9972d034008536e
Sergey Volkov <s.volkov@netris.ru>
2019-04-24 17:09:03 +0300

    Renew module structure. Fix build. Bump version

---
+++ ios/Classes/TiCustomphotoalbumModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiCustomphotoalbumModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TiCustomphotoalbumModuleAssets
+
+- (NSData *)moduleAsset
+{
+  
+
+  return nil;
+}
+
+- (NSData *)resolveModuleAsset:(NSString *)path
+{
+  
+
+  return nil;
+}
+
+@end
